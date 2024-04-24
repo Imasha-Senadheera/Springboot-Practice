4cbbb31d171cb6af227a4e129766e314ef175df3
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-04-24 11:39:42 +0530

    3rd Commit

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,13 +1,30 @@
 package com.example.demo.student;
 
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
     private LocalDate dob;
     private int age;
+
+    // No-argument constructor
+    public Student() {
+    }
 
     public Student(Long id, String name, String email, LocalDate dob, int age) {
         this.id = id;
@@ -16,6 +33,7 @@ public class Student {
         this.dob = dob;
         this.age = age;
     }
+
 
     // Getters and Setters
 
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,25 +1,24 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-
 public class StudentService {
 
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Imashaaaa",
-                        "@imasha@gmail.com",
-                        LocalDate.of(2000, Month.MARCH, 26),
-                        21
-                )
-        );
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(
+                1L,
+                "Imashaaaa",
+                "@imasha@gmail.com",
+                LocalDate.of(2000, Month.MARCH, 26),
+                21
+        ));
+        return students;
     }
 }
