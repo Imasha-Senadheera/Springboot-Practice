0c639e2ba00cc1e5bafb011e614f63832c3adb4d
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-04-24 16:16:56 +0530

    4th Commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student imasha = new Student(
+                    1L,
+                    "Imasha",
+                    "@imasha@gmail.com",
+                    LocalDate.of(2000, MARCH, 26)
+
+            );
+
+            Student kasun = new Student(
+                    2L,
+                    "Kasun",
+                    "@Kasun@gmail.com",
+                    LocalDate.of(1993, AUGUST, 17)
+            );
+
+            repository.save(imasha);
+            repository.save(kasun);
+        };
+    }
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -10,15 +11,14 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student(
-                1L,
-                "Imashaaaa",
-                "@imasha@gmail.com",
-                LocalDate.of(2000, Month.MARCH, 26),
-                21
-        ));
-        return students;
+        return studentRepository.findAll();
     }
 }
